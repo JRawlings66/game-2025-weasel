63f8c80ae761879f8c11a98a476dda8a87eb1469
Jacob Rawlings <122933457+JRawlings66@users.noreply.github.com>
2025-01-02 23:43:32 -0800

    added grid placement scripts V1

---
+++ Scripts/PlacementSystem.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlacementSystem : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject mouseIndicator, cellIndicator;
+    [SerializeField]
+    private InputManager inputManager;
+    [SerializeField]
+    private Grid grid;
+
+    [SerializeField]
+    private ObjectsDatabaseSO database;
+    private int selectedObjectIndex = -1;
+
+    [SerializeField]
+    private GameObject gridVisualization;
+
+    private void Start()
+    {
+        StopPlacement();
+    }
+
+    public void StartPlacement(int ID)
+    {
+        StopPlacement();
+        selectedObjectIndex = database.objectsData.FindIndex(data => data.ID == ID);
+        // If we dont have any object that matches the data ID, FindIndex returns -1
+        if(selectedObjectIndex < 0)
+        {
+            Debug.LogError($"No ID found {ID}");
+            return;
+        }
+        // If we have a valid ID
+        gridVisualization.SetActive(true);
+        cellIndicator.SetActive(true);
+        inputManager.OnClicked += PlaceStructure;
+        inputManager.OnExit += StopPlacement;
+    }
+
+    private void PlaceStructure()
+    {
+        // When we press LMB over the grid, instantiate new object and place at position of cell
+        if(inputManager.IsPointerOverUI())
+        {
+            return;
+        }
+        // get mouse and grid position
+        Vector3 mousePosition = inputManager.GetSelectedMapPosition();
+        Vector3Int gridPosition = grid.WorldToCell(mousePosition);
+        // get the selected game object prefab
+        GameObject newObject = Instantiate(database.objectsData[selectedObjectIndex].Prefab);
+        newObject.transform.position = grid.CellToWorld(gridPosition);
+    }
+
+    private void StopPlacement()
+    {
+        selectedObjectIndex = -1;
+        gridVisualization.SetActive(false);
+        cellIndicator.SetActive(false);
+        inputManager.OnClicked -= PlaceStructure;
+        inputManager.OnExit -= StopPlacement;
+    }
+
+    private void Update()
+    {
+        // first check to see if we are in 'placement mode'
+        if(selectedObjectIndex < 0)
+            return;
+        // get mouse and grid position
+        Vector3 mousePosition = inputManager.GetSelectedMapPosition();
+        Vector3Int gridPosition = grid.WorldToCell(mousePosition);
+        mouseIndicator.transform.position = mousePosition;
+        cellIndicator.transform.position = grid.CellToWorld(gridPosition);
+    }
+}
